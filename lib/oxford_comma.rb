43055bf7d5aedded2cc655081ea04101bd78c233
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-11 18:44:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   when 2
     "#{array[0]} and #{array[1]}"
   else
-    array[-1] << "and #{array.last}"
+    array[-1] = "and #{array[-1]}"
     array.join(", ")
   end
 end
